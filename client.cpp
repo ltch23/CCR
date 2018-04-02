2e5bb3fb2d09c9b144ad34b0a94a745b937abe91
ltch23 <luistong23@gmail.com>
2018-04-01 20:42:16 -0500

    action= Print, Login, Chat and Response

--- client.cpp
+++ client.cpp
@@ -14,8 +14,21 @@
 
 using namespace std;
 
-int nbites = 4;
 vector<thread> T; // first,second;
+struct sockaddr_in stSockAddr;
+int Res;
+int SocketFD ;
+int n;
+char buffer[255];
+
+void fillZeros(string & st, int nroBytes){
+	string aux = to_string(st.size());
+	int dif = nroBytes - int(aux.size());
+	st = aux + st;
+	for (int i = 0; i < dif; i++)
+		st = "0" + st;
+}
+
 void read2(int SocketFD, char *buffer)
 {
 	int n;
@@ -24,38 +37,92 @@ void read2(int SocketFD, char *buffer)
 		bzero(buffer, 100);
 		do
 		{
+			// cout<<"Reading\n";
 			n = read(SocketFD, buffer, 4);
-			n = read(SocketFD, buffer, atoi(buffer));
+			string size_msg(buffer);
+			bzero(buffer, 4);
+
+			n = read(SocketFD, buffer, 1);
+			string action(buffer);
+			bzero(buffer, 1);
+
+			if (action == "R"){
+				n = read(SocketFD, buffer, atoi(size_msg.c_str()));
+			}
+			
+			else if (action == "D"){
+
+			}
+
+			
+
+			// n = read(SocketFD, buffer, atoi(buffer));
 		} while (n == 0);
-		printf("client: %s\n", buffer);
+		cout << buffer << endl;
 	}
 }
 
-void write2(int SocketFD)
+void write2(int  SocketFD)
 {
-	string st = "", aux = "";
+	string msg = "", aux = "", op = "";
 	int dif = 0;
+
 	while (1)
 	{
-		// cout << "server: ";
-		getline(cin, st);
-		aux = to_string(st.size());
-		dif = nbites - int(aux.size());
-		st = aux + st;
-		for (int i = 0; i < dif; i++)
-			st = "0" + st;
-		write(SocketFD, st.c_str(), st.size());
+		cout << "------Menu (action)-----\n"
+			 << "P -> Print list of user on the chat \n"
+			 << "L -> Login to the char\n"
+			 << "C -> Send a msg to a user on the chat\n"
+			 << "E -> End chat or logout from chat\n"
+			 << "F -> Send a file from a user to another user\n"
+			 << "----------------------------\n"
+			 << endl;
+		cin >> op;
+
+		if (op == "P")
+		{
+			msg = "0000P";
+		}
+
+		else if (op == "L")
+		{
+			string username = "";
+			cout << "enter nickname: ";
+			// getline(cin, username);
+			cin >> username;
+			msg="L"+username;
+			fillZeros(msg,4);
+
+		}
+		else if (op == "C"){
+			string othername="";
+			cout<<"enter nickname to chat: ";
+			cin>>othername;
+			fillZeros(othername,2);
+			cout<<"enter message: ";
+			cin.ignore();
+			getline(cin,msg);
+			msg="C"+othername+msg;
+			fillZeros(msg,4);
+		}
+		else if (op == "E"){
+			msg = "0000E";
+		}
+		else if (op == "F"){
+		}
+		else{
+			cout << "error action no find\n";
+			break;
+		}
+		write(SocketFD, msg.c_str(), msg.size());
 	}
 
 	// cout << "sali de write" << endl;
 }
 
-int main(void)
-{
-	struct sockaddr_in stSockAddr;
-	int Res;
-	int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	int n;
+int main(){
+	
+	SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	if (-1 == SocketFD)
 	{
@@ -66,7 +133,7 @@ int main(void)
 	memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
 
 	stSockAddr.sin_family = AF_INET;
-	stSockAddr.sin_port = htons(1000);
+	stSockAddr.sin_port = htons(1100);
 	Res = inet_pton(AF_INET, "192.168.0.6", &stSockAddr.sin_addr);
 
 	if (0 > Res)
@@ -88,15 +155,13 @@ int main(void)
 		close(SocketFD);
 		exit(EXIT_FAILURE);
 	}
-	char buffer[255];
-	//n = write(SocketFD,"Hi, whore",18);
-	/* perform read write operations ... */
 	n = read(SocketFD, buffer, 9);
 	printf("%s\n", buffer);
 	T.push_back(thread(read2, SocketFD, buffer));
 	T.push_back(thread(write2, SocketFD));
 	T[0].join();
 	T[1].join();
+
 	shutdown(SocketFD, SHUT_RDWR);
 	close(SocketFD);
 	return 0;
--- server.cpp
+++ server.cpp
@@ -11,50 +11,160 @@
 #include <unistd.h>
 #include <thread>
 #include <vector>
+#include <map>
 #include <chrono>
 
 
 using namespace std;
 
-int nbites = 4;
-void read2(int SocketFD, char *buffer)
-{
-	int n;
-	for (;;)
-	{
-		bzero(buffer, 255);
-		do
-		{
-			n = read(SocketFD, buffer, 4);
-			n = read(SocketFD, buffer, atoi(buffer));
-		} while (n == 0);
-		printf("client(Cutire): %s\n", buffer);
-	}
+int nbites_4 = 4;
+struct sockaddr_in stSockAddr;
+int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+char buffer[256];
+int n;
+map<string,int> clients;
+
+void acceptClient(int ConnectFD);
+void write2(int ConnectFD,string prnt, string act);
+void read2(int ConnectFD, char *buffer);
+
+void fillZeros(string &st, int nroBytes){
+	string aux = to_string(st.size());
+	int dif = nroBytes - int(aux.size());
+	st = aux + st;
+	for (int i = 0; i < dif; i++)
+		st = "0" + st;
 }
 
-void write2(int SocketFD)
+bool find_repeat(string st){
+
+	map<string, int>::iterator it;
+	for (it = clients.begin(); it != clients.end(); it++)
+		if(it->first == st){
+			return true;
+		}
+	return false;
+}
+bool find_str(int id,string & st){
+
+	map<string, int>::iterator it;
+	for (it = clients.begin(); it != clients.end(); it++)
+		if(it->second == id){
+			st=it->first;
+		}
+}
+
+
+void read2(int ConnectFD, char *buffer)
 {
-	string st,aux="";
-	int dif=0;
-	for (;;)
-	{
-		// cout << "server: ";
-		getline(cin,st);
-		aux = to_string(st.size());
-		dif = nbites - int(aux.size());
-		st = aux+st;
-		for(int i=0;i<dif;i++)
-			st="0"+st;
-		write(SocketFD, st.c_str(), st.size());
+	int n;
+
+	for (;;){
+		bzero(buffer, 255);
+		do{
+			n = read(ConnectFD, buffer, 4);
+			int size_txt=atoi(buffer);
+			bzero(buffer, 4);
+
+			n = read(ConnectFD, buffer, 1);
+			string action(buffer);
+			bzero(buffer, 1);
+
+			if (action == "P"){
+				
+				string prnt="";
+				map<string, int>::iterator it;
+				for (it = clients.begin(); it != clients.end(); it++){
+					prnt+="username: "+it->first
+					+" value: " + to_string(it->second)+"\n";
+				}
+				cout<<"Print: "<<prnt<<endl;
+				write2(ConnectFD,prnt,action);
+			}
+
+			else if (action == "L"){
+				
+				if(find_repeat(string(buffer)) == true){
+					string err="nickname already exists, enter other\n";
+					write2(ConnectFD,err.c_str(),action);
+				}
+			= read(ConnectFD, buffer, size_txt);
+
+				clienuffer] = ConnectFD;
+				cout << "Login: " << buffer << endl;
+			}
+
+			else if (action == "C"){
+
+				string username = "";
+				find_str(ConnectFD,username);
+
+				n = read(ConnectFD, buffer, 2);
+				int size_othername=atoi(buffer);
+				bzero(buffer, 2);
+
+				n = read(ConnectFD, buffer, size_othername);
+				string othername(buffer);
+				bzero(buffer, size_othername);
+
+				int size_msg= size_txt-3-othername.size();
+				// cout << "size_msg: " << size_msg<<endl;
+				n = read(ConnectFD, buffer, size_msg);
+				string msg(buffer);
+				bzero(buffer,size_msg);
+				
+				//buscando
+				int otherConnectFD = clients.find(othername)->second;
+				if (otherConnectFD < 0){
+					perror("error in nickname");
+				}
+				
+				msg = username+": "+msg;
+				cout<<msg+" -> "+othername<<endl;
+				write2(otherConnectFD, msg, action);
+			}
+			else if (action == "E"){
+				cout<<"End\n";
+			}
+			else if (action == "F"){
+						
+			}
+			else
+			cout << "error in action, msg bad\n";
+
+		} while (n == 0);
+		// printf("client: %s\n", buffer);
 	}
-	// cout << "sali de write" << endl;
+}
+void write2(int ConnectFD, string mssg,string act)
+{
+
+	if (act == "P" or act == "C" or act == "L")
+	{
+		mssg = "R" +mssg;
+		fillZeros(mssg,4);
+		write(ConnectFD, mssg.c_str(), mssg.size());
+	}
+
+}
+void acceptClient(int ConnectFD)
+{
+	if (0 > ConnectFD)
+	{
+		perror("error accept failed");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	write(ConnectFD, "Conectado", 9);
+	bzero(buffer, 256);
+	//changing to detach();
+	thread(read2, ConnectFD,buffer).detach();
+
+	this_thread::sleep_for(chrono::seconds(100));
 }
 
 int main(void){
-	struct sockaddr_in stSockAddr;
-	int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	char buffer[256];
-	int n;
 
 	if(-1 == SocketFD){
 		perror("can not create socket");
@@ -64,7 +174,7 @@ int main(void){
 	memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
 
 	stSockAddr.sin_family = AF_INET;
-	stSockAddr.sin_port = htons(1000);
+	stSockAddr.sin_port = htons(1100);
 	stSockAddr.sin_addr.s_addr = INADDR_ANY;
 
 	if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in))){
@@ -78,22 +188,12 @@ int main(void){
 		close(SocketFD);
 		exit(EXIT_FAILURE);
 	}
-	int ConnectFD = accept(SocketFD, NULL, NULL);
-	if(0 > ConnectFD){
-		perror("error accept failed");
-		close(SocketFD);
-		exit(EXIT_FAILURE);
+	
+	while(true){
+		int ConnectFD = accept(SocketFD, NULL, NULL);
+		// cout << "ConnectFD: " << ConnectFD << endl;
+		thread(acceptClient,ConnectFD).detach();
 	}
-
-	/* perform read write operations ... */
-	vector<thread> T;
-	write(ConnectFD,"Conectado",9);
-	bzero(buffer, 256);
-	//changing to detach();
-	thread(write2, ConnectFD).detach();
-	thread(read2, ConnectFD,buffer).detach();
-
-	this_thread::sleep_for(chrono::seconds(100));
 
 	shutdown(SocketFD, SHUT_RDWR);
 	close(SocketFD);
